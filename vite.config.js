e25ea49cd0e8c320debedcadeb46157b09349769
ZalaManish187 <160453479+ZalaManish187@users.noreply.github.com>
2024-11-08 18:45:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/<TOMATO-WEBSITE>/', // Replace <repository-name> with your repo name
+  base: '/frontend/',  // Correct repository name
   plugins: [react()],
 });
